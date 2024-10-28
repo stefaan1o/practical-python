5208e04154d844d0b9d2d4664eb83c651fab325c
stefaan van eenoo <stefaan.vaneenoo@gmail.com>
2024-10-28 15:42:01 +0100

    feat: chapter 5 exercises - finished

---
+++ Work/5_7/stock.py
@@ -0,0 +1,25 @@
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def __repr__(self):
+        return f'Stock({self.name},{self.shares},{self.price}'
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected an int')
+        self._shares = value
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
